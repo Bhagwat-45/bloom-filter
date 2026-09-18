fcbcfd5d2f07eef8fe98476ed6b1cefe8d5b33e5
Chinmay Bhagwat <cbh.mavim@gmail.com>
2026-09-18 12:16:22 +0530

    done

--- main.py
+++ main.py
@@ -9,11 +9,22 @@ bits = [0] * m
 
 def h1(s):
     # TODO: return sum of bytes mod m
+    total = 0
+    if s:
+        for char in s:
+            total += ord(char)
+        return total%m
     return 0
 
 def h2(s):
     # TODO: return sum of (byte * (position+1)) mod m
     #   for "apple"  ->  97*1 + 112*2 + 112*3 + 108*4 + 101*5  mod 64
+    total = 0
+    if s:
+        for i,char in enumerate(s): 
+            total += ord(char)*(i+1)
+        return total%m
+            
     return 0
 
 out = []
@@ -27,12 +38,19 @@ for raw in sys.stdin:
 
     if cmd == "ADD":
         # TODO: set bits[h1(arg)] and bits[h2(arg)] to 1, then output "OK"
-        pass
+        bits[h1(arg)] = 1
+        bits[h2(arg)] = 1
+        print("OK")
+        
     elif cmd == "CHECK":
         # TODO: if BOTH bits are 1, output "MAYBE"; otherwise output "NO"
+        if bits[h1(arg)] == 1 and bits[h2(arg)] == 1:
+            print("MAYBE")
+        else:
+            print("NO")
         pass
     elif cmd == "BITS":
         # TODO: output the bit array as a 64-character string
-        pass
+        print("".join(str(b) for b in bits))
 
 sys.stdout.write("\n".join(out) + "\n")
